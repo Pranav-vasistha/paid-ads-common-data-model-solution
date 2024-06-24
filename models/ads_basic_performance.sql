68ef4f58756cd3ab2f496d0c91751e88fc36fc84
Pranav Vasistha <pv.vasistha@gmail.com>
2024-06-24 17:46:48 +0000

    Final

--- models/ads_basic_performance.sql
+++ models/ads_basic_performance.sql
@@ -14,12 +14,6 @@ WITH alldata AS (
         CAST(spend AS INT64) AS spend,
         CAST(conversions AS INT64) AS conversions,
         CAST(engagement AS INT64) AS engagement,
-        CAST(engagement_cost AS FLOAT64) AS engagement_cost,
-        CAST(conversion_cost AS FLOAT64) AS conversion_cost,
-        CAST(cpc AS FLOAT64) AS cpc,
-        CAST(ctr AS FLOAT64) AS ctr,
-        CAST(cpi AS FLOAT64) AS cpi,
-        CAST(engagement_rate AS FLOAT64) AS engagement_rate,
         CAST(video_views AS INT64) AS video_views
     FROM {{ ref('bing_data') }}
     UNION ALL
@@ -36,12 +30,6 @@ WITH alldata AS (
         CAST(spend AS INT64) AS spend,
         CAST(conversions AS INT64) AS conversions,
         CAST(engagement AS INT64) AS engagement,
-        CAST(engagement_cost AS FLOAT64) AS engagement_cost,
-        CAST(conversion_cost AS FLOAT64) AS conversion_cost,
-        CAST(cpc AS FLOAT64) AS cpc,
-        CAST(ctr AS FLOAT64) AS ctr,
-        CAST(cpi AS FLOAT64) AS cpi,
-        CAST(engagement_rate AS FLOAT64) AS engagement_rate,
         CAST(video_views AS INT64) AS video_views
     FROM {{ ref('twitter_data') }}
     UNION ALL
@@ -58,12 +46,6 @@ WITH alldata AS (
         CAST(spend AS INT64) AS spend,
         CAST(conversions AS INT64) AS conversions,
         CAST(engagement AS INT64) AS engagement,
-        CAST(engagement_cost AS FLOAT64) AS engagement_cost,
-        CAST(conversion_cost AS FLOAT64) AS conversion_cost,
-        CAST(cpc AS FLOAT64) AS cpc,
-        CAST(ctr AS FLOAT64) AS ctr,
-        CAST(cpi AS FLOAT64) AS cpi,
-        CAST(engagement_rate AS FLOAT64) AS engagement_rate,
         CAST(video_views AS INT64) AS video_views
     FROM {{ ref('tiktok_data') }}
     UNION ALL
@@ -80,12 +62,6 @@ WITH alldata AS (
         CAST(spend AS INT64) AS spend,
         CAST(conversions AS INT64) AS conversions,
         CAST(engagement AS INT64) AS engagement,
-        CAST(engagement_cost AS FLOAT64) AS engagement_cost,
-        CAST(conversion_cost AS FLOAT64) AS conversion_cost,
-        CAST(cpc AS FLOAT64) AS cpc,
-        CAST(ctr AS FLOAT64) AS ctr,
-        CAST(cpi AS FLOAT64) AS cpi,
-        CAST(engagement_rate AS FLOAT64) AS engagement_rate,
         CAST(video_views AS INT64) AS video_views
     FROM {{ ref('facebook_data') }}
 )
