5046d3b6efea36a1872ff0e9cf1d89fd678ba74d
Pranav Vasistha <pv.vasistha@gmail.com>
2024-06-24 10:27:28 +0000

    tiktok error1

--- models/tiktok_data.sql
+++ models/tiktok_data.sql
@@ -1,6 +1,6 @@
--- SQL script for TikTok Ads data processing
+-- models/tiktok_data.sql
 
-WITH tiktok_data AS (
+WITH rename_tiktok AS (
     SELECT
         __insert_date AS insert_date,
         ad_id,
@@ -11,21 +11,42 @@ WITH tiktok_data AS (
         clicks,
         date,
         impressions,
-        (rt_installs + skan_app_install + registrations) AS engagement, -- Engagement defined as sum of installs and registrations
+        rt_installs,
+        skan_app_install,
+        registrations,
         ad_status,
         ad_text AS creative_body,
         spend,
         video_views,
-        (conversions + skan_conversion) AS conversions, -- Conversions defined as sum of conversions, and skan_conversion
+        (rt_installs + skan_app_install + registrations) AS engagement, -- Engagement defined as sum of installs and registrations
+        (conversions + skan_conversion) AS conversions -- Conversions defined as sum of conversions and skan_conversion
+    FROM 
+        {{ ref('src_ads_tiktok_ads_all_data') }}
+),
+tiktok_data AS (
+    SELECT
+        insert_date,
+        ad_id,
+        add_to_cart,
+        adset_id,
+        campaign_id,
+        channel,
+        clicks,
+        date,
+        impressions,
+        ad_status,
+        creative_body,
+        spend,
+        video_views,
         ROUND(spend / NULLIF(clicks, 0), 2) AS cpc,
         ROUND((clicks / NULLIF(impressions, 0)) * 100, 2) AS ctr,
         ROUND(spend / NULLIF(impressions, 0), 2) AS cpi,
         ROUND((engagement / NULLIF(impressions, 0)) * 100, 2) AS engagement_rate,
         ROUND(spend / NULLIF(engagement, 0), 2) AS engagement_cost,
         ROUND(spend / NULLIF(conversions, 0), 2) AS conversion_cost
-    FROM 
-        {{ ref('src_ads_tiktok_ads_all_data') }}
+    FROM
+        rename_tiktok
 )
 
 SELECT *
-FROM tiktok_data;
+FROM tiktok_data
