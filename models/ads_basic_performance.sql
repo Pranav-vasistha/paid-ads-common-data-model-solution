7f628025ea800d8e6f325471fab39d57ac73b262
Pranav Vasistha <pv.vasistha@gmail.com>
2024-06-24 15:43:52 +0000

    changes

--- models/ads_basic_performance.sql
+++ models/ads_basic_performance.sql
@@ -1,36 +1,97 @@
 {{ config(materialized='view') }}
 
-WITH alldata
-AS 
-SELECT 
-    CAST(insert_date AS DATETIME) AS insert_date,
-    CAST(ad_id AS STRING) AS ad_id,
-    CAST(adset_id AS STRING) AS adset_id,
-    CAST(campaign_id AS STRING) AS campaign_id,
-    CAST(channel AS STRING) AS channel,
-    CAST(creative_title AS STRING) AS creative_title,
-    CAST(clicks AS INT64) AS clicks,
-    CAST(date AS DATE) AS date,
-    CAST(impressions AS INT64) AS impressions,
-    CAST(revenue AS INT64) AS revenue,
-    CAST(spend AS INT64) AS spend,
-    CAST(conversions AS INT64) AS conversions,
-    CAST(engagement AS INT64) AS engagement,
-    CAST(engagement_cost AS FLOAT64) AS engagement_cost,
-    CAST(conversion_cost AS FLOAT64) AS conversion_cost,
-    CAST(cpc AS FLOAT64) AS cpc,
-    CAST(ctr AS FLOAT64) AS ctr,
-    CAST(cpi AS FLOAT64) AS cpi,
-    CAST(engagement_rate AS FLOAT64) AS engagement_rate,
-    CAST(video_views AS INT64) AS video_views
-FROM (
-    {{ ref('bing_data') }}
-    UNION
-    {{ ref('twitter_data') }}
-    UNION
-    {{ ref('tiktok_data') }}
-    UNION
-    {{ ref('facebook_data') }}
+WITH alldata AS (
+    SELECT 
+        CAST(insert_date AS DATETIME) AS insert_date,
+        CAST(ad_id AS STRING) AS ad_id,
+        CAST(adset_id AS STRING) AS adset_id,
+        CAST(campaign_id AS STRING) AS campaign_id,
+        CAST(channel AS STRING) AS channel,
+        CAST(creative_title AS STRING) AS creative_title,
+        CAST(clicks AS INT64) AS clicks,
+        CAST(date AS DATE) AS date,
+        CAST(impressions AS INT64) AS impressions,
+        CAST(revenue AS INT64) AS revenue,
+        CAST(spend AS INT64) AS spend,
+        CAST(conversions AS INT64) AS conversions,
+        CAST(engagement AS INT64) AS engagement,
+        CAST(engagement_cost AS FLOAT64) AS engagement_cost,
+        CAST(conversion_cost AS FLOAT64) AS conversion_cost,
+        CAST(cpc AS FLOAT64) AS cpc,
+        CAST(ctr AS FLOAT64) AS ctr,
+        CAST(cpi AS FLOAT64) AS cpi,
+        CAST(engagement_rate AS FLOAT64) AS engagement_rate,
+        CAST(video_views AS INT64) AS video_views
+    FROM {{ ref('bing_data') }}
+    UNION ALL
+    SELECT 
+        CAST(insert_date AS DATETIME) AS insert_date,
+        CAST(ad_id AS STRING) AS ad_id,
+        CAST(adset_id AS STRING) AS adset_id,
+        CAST(campaign_id AS STRING) AS campaign_id,
+        CAST(channel AS STRING) AS channel,
+        CAST(creative_title AS STRING) AS creative_title,
+        CAST(clicks AS INT64) AS clicks,
+        CAST(date AS DATE) AS date,
+        CAST(impressions AS INT64) AS impressions,
+        CAST(revenue AS INT64) AS revenue,
+        CAST(spend AS INT64) AS spend,
+        CAST(conversions AS INT64) AS conversions,
+        CAST(engagement AS INT64) AS engagement,
+        CAST(engagement_cost AS FLOAT64) AS engagement_cost,
+        CAST(conversion_cost AS FLOAT64) AS conversion_cost,
+        CAST(cpc AS FLOAT64) AS cpc,
+        CAST(ctr AS FLOAT64) AS ctr,
+        CAST(cpi AS FLOAT64) AS cpi,
+        CAST(engagement_rate AS FLOAT64) AS engagement_rate,
+        CAST(video_views AS INT64) AS video_views
+    FROM {{ ref('twitter_data') }}
+    UNION ALL
+    SELECT 
+        CAST(insert_date AS DATETIME) AS insert_date,
+        CAST(ad_id AS STRING) AS ad_id,
+        CAST(adset_id AS STRING) AS adset_id,
+        CAST(campaign_id AS STRING) AS campaign_id,
+        CAST(channel AS STRING) AS channel,
+        CAST(creative_title AS STRING) AS creative_title,
+        CAST(clicks AS INT64) AS clicks,
+        CAST(date AS DATE) AS date,
+        CAST(impressions AS INT64) AS impressions,
+        CAST(revenue AS INT64) AS revenue,
+        CAST(spend AS INT64) AS spend,
+        CAST(conversions AS INT64) AS conversions,
+        CAST(engagement AS INT64) AS engagement,
+        CAST(engagement_cost AS FLOAT64) AS engagement_cost,
+        CAST(conversion_cost AS FLOAT64) AS conversion_cost,
+        CAST(cpc AS FLOAT64) AS cpc,
+        CAST(ctr AS FLOAT64) AS ctr,
+        CAST(cpi AS FLOAT64) AS cpi,
+        CAST(engagement_rate AS FLOAT64) AS engagement_rate,
+        CAST(video_views AS INT64) AS video_views
+    FROM {{ ref('tiktok_data') }}
+    UNION ALL
+    SELECT 
+        CAST(insert_date AS DATETIME) AS insert_date,
+        CAST(ad_id AS STRING) AS ad_id,
+        CAST(adset_id AS STRING) AS adset_id,
+        CAST(campaign_id AS STRING) AS campaign_id,
+        CAST(channel AS STRING) AS channel,
+        CAST(creative_title AS STRING) AS creative_title,
+        CAST(clicks AS INT64) AS clicks,
+        CAST(date AS DATE) AS date,
+        CAST(impressions AS INT64) AS impressions,
+        CAST(revenue AS INT64) AS revenue,
+        CAST(spend AS INT64) AS spend,
+        CAST(conversions AS INT64) AS conversions,
+        CAST(engagement AS INT64) AS engagement,
+        CAST(engagement_cost AS FLOAT64) AS engagement_cost,
+        CAST(conversion_cost AS FLOAT64) AS conversion_cost,
+        CAST(cpc AS FLOAT64) AS cpc,
+        CAST(ctr AS FLOAT64) AS ctr,
+        CAST(cpi AS FLOAT64) AS cpi,
+        CAST(engagement_rate AS FLOAT64) AS engagement_rate,
+        CAST(video_views AS INT64) AS video_views
+    FROM {{ ref('facebook_data') }}
 )
 
 SELECT * FROM alldata
