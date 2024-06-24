a850e13d6547f599f619bc0ade352a7cc4140d5c
Pranav Vasistha <pv.vasistha@gmail.com>
2024-06-24 10:14:37 +0000

    bing error solving

--- models/bing_data.sql
+++ models/bing_data.sql
@@ -1,7 +1,8 @@
--- SQL script for Bing Ads data processing
-WITH bing_data AS (
+-- models/bing_data.sql
+
+WITH rename_bing AS (
     SELECT
-        insert_date,
+        __insert_date AS insert_date,
         ad_id,
         adset_id,
         campaign_id,
@@ -11,16 +12,37 @@ WITH bing_data AS (
         title_part_2,
         clicks,
         date,
-        imps AS impressions,
+        imps AS impressions,  -- Renaming imps to impressions
         revenue,
         spend,
-        conv AS conversions,
-        0 AS engagement_cost,  -- No engagements value, setting to 0
-        ROUND(spend / NULLIF(conv, 0), 2) AS conversion_cost,
-        ROUND(spend / NULLIF(clicks, 0), 2) AS cpc,
-        ROUND((clicks / NULLIF(impressions, 0)) * 100, 2) AS ctr,
-        ROUND(spend / NULLIF(impressions, 0), 2) AS cpi,
-        0 AS engagement_rate  -- No engagements value, setting to 0
+        conv AS conversions
     FROM 
-        {{ ref('src_ads_bing_all_data') }}
+        `paid-ads-model`.`dbt_pvasistha`.`src_ads_bing_all_data`
+),
+bing_data AS (
+    SELECT
+        insert_date,
+        ad_id,
+        adset_id,
+        campaign_id,
+        channel,
+        creative_title,
+        title_part_1,
+        title_part_2,
+        clicks,
+        date,
+        impressions,
+        revenue,
+        spend,
+        conversions,
+        0 AS engagement_cost,  -- No engagements value, setting to 0
+        ROUND(spend / NULLIF(conversions, 0), 2) AS conversion_cost,
+        ROUND(spend / NULLIF(clicks, 0), 2) AS cpc,
+        ROUND((clicks / NULLIF(impressions, 0)) * 100, 2) AS ctr,  -- Using impressions
+        ROUND(spend / NULLIF(impressions, 0), 2) AS cpi,  -- Using impressions
+        0 AS engagement_rate  -- No engagements value, setting to 0
+    FROM
+        rename_bing
 )
+
+SELECT * FROM bing_data
